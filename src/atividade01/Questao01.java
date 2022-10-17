1b916a3c7c0a3b56ff1c62679f0de8c9d8777111
luiz felipe <luizfelipe.freire@gmail.com>
2022-10-17 20:24:44 -0300

    resolução lista de atividade 01 questão 1

---
+++ src/atividade01/Questao01.java
@@ -0,0 +1,21 @@
+package atividade01;
+
+import java.util.Scanner;
+
+public class Questao01 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite 1 numero:");
+        double nota01 = ler.nextDouble();
+        System.out.println("Digite 1 numero:");
+        double nota02 = ler.nextDouble();
+        ler.close();
+        double media = (nota01 + nota02) / 2;
+        System.out.println("Média Final: " + media);
+        if (media >= 7) {
+            System.out.println("Aprovado");
+        } else {
+            System.out.println("Reprovado");
+        }
+    }
+}
---
+++ src/atividade01/Questao01Plus.java
@@ -0,0 +1,26 @@
+package atividade01;
+
+import java.util.Scanner;
+
+public class Questao01Plus {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite 1 numero:");
+        double nota01 = ler.nextDouble();
+        System.out.println("Digite 1 numero:");
+        double nota02 = ler.nextDouble();
+        ler.close();
+        double media = (nota01 + nota02) / 2;
+        System.out.println("Média Final: " + media);
+        // aprovado >= 7, recuperação >= 4 , reprovado < 4
+        if (media >= 7) {
+            System.out.println("Aprovado");
+        } else {
+            if (media >= 4) {
+                System.out.println("Recuperação");
+            } else {
+                System.out.println("Reprovado");
+            }
+        }
+    }
+}

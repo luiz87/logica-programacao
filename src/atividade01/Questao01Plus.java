c62eca191cdd6c97c4954955d4b3cafdc7af2f09
luiz felipe <luizfelipe.freire@gmail.com>
2022-10-17 20:45:57 -0300

    nova regra para erro de digitação

--- src/atividade01/Questao01Plus.java
+++ src/atividade01/Questao01Plus.java
@@ -11,14 +11,16 @@ public class Questao01Plus {
         double nota02 = ler.nextDouble();
         ler.close();
         double media = (nota01 + nota02) / 2;
-        System.out.println("Média Final: " + media);
-        // aprovado >= 7, recuperação >= 4 , reprovado < 4
-        if (media >= 7) {
+        System.out.println("Média Final : " + media);
+        // aprovado >= 7, recuperação >= 4 , reprovado < 4, erro digitação > 10 ou < 0
+        if (media >= 7 && media <= 10) { 
             System.out.println("Aprovado");
-        } else if (media >= 4) {
+        } else if (media >= 4 && media < 7) {
             System.out.println("Recuperação");
-        } else {
+        } else if (media < 4 && media >= 0){
             System.out.println("Reprovado");
+        } else {
+            System.out.println("Erro digitação");
         }
 
     }

4ffd4e5f388e8f3dc5c83e1630cfedafc030c62d
luiz felipe <luizfelipe.freire@gmail.com>
2022-10-31 20:28:09 -0300

    Exemplo POO

---
+++ src/exemplos/exemplosPoo/Caneta.java
@@ -0,0 +1,22 @@
+package exemplos.exemplosPoo;
+
+public class Caneta {
+
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampa;
+
+    void rabiscar(){
+        if (tampa) {
+            System.out.println("Rabisco");
+        } else {
+            System.out.println("Retire a tampa");
+        }
+    }
+
+    void tampar(){
+        tampa = true;
+    }
+}
